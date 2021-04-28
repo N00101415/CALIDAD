84cab63d4e1c73e0bb5f7d50a7eca419925f5f12
kiuby101 <70725678+kiuby101@users.noreply.github.com>
2021-04-28 01:35:27 -0500

    version 1.0
    clases principales

---
+++ CALIDAD/TakahashiProyectosyServicios/src/main/java/com/grupo4/demo/models/entity/Categoria.java
@@ -0,0 +1,76 @@
+package com.grupo4.demo.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable{
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String codigo;
+	private String nombre;
+	private String tipoArticulo;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getTipoArticulo() {
+		return tipoArticulo;
+	}
+
+
+
+	public void setTipoArticulo(String tipoArticulo) {
+		this.tipoArticulo = tipoArticulo;
+	}
+
+
+
+	private static final long serialVersionUID = 1L;
+	
+	
+}
